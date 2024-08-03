38b1303a3cc5098bde0b13f52d333fb9e1a5e276
Sayendip1001 <sayendipdas@gmail.com>
2024-08-04 02:11:21 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex flex-col sm:flex-row justify-between items-center sm:items-center px-5 py-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30 bg-white">
+      {/* logo and site name */}
+      <div className="flex justify-between items-center">
+        {/* I will add the image here later */}
+        <img src="/picprismlogo.png" alt="our logo" className="w-[70px]" />
+        <Link to="/" className="font-bold text-4xl ">
+          PicPrism
+        </Link>
+      </div>
+
+      {/* list of other tabs */}
+
+      <ul className="flex gap-5 text-[22px] font-semibold text-gray-500 ">
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">
+          About
+        </Link>
+        <Link to="/" className="hover:text-black cursor-pointer sm:p-2">
+          Contact
+        </Link>
+        <Link to="/login" className="hover:text-black cursor-pointer sm:p-2">
+          Log In
+        </Link>
+        <Link to="/signup" className="hover:text-black cursor-pointer sm:p-2">
+          Sign Up
+        </Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
